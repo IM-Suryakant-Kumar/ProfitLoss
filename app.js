352dba972b66d3fccf41c6f68418f730d040670e
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-02-14 14:36:54 +0530

    docs: ex08: bonus (Optional)

--- app.js
+++ app.js
@@ -12,15 +12,18 @@ tellMeBtn.addEventListener("click", () => {
   if (initPriceValue === 0 || currPriceValue === 0 || numOfStocksValue === 0) {
     alert("Please fill out all Fields");
     return;
-  }
-
-  if (initPriceValue > currPriceValue) {
+  } else if (initPriceValue > currPriceValue) {
     const loss = (initPriceValue - currPriceValue) * numOfStocksValue;
     const lossPercentage = ((loss / initPriceValue) * 100).toFixed(2);
+    output.style.color = "red";
     console.log("loss", loss, "lossPercentage", lossPercentage);
   } else if (initPriceValue < currPriceValue) {
     const profit = (currPriceValue - initPriceValue) * numOfStocksValue;
     const profitPercentage = ((profit / initPriceValue) * 100).toFixed(2);
+    output.style.color = "green";
     console.log("profit", profit, "profitPercentage", profitPercentage);
+  } else {
+    output.style.color = "white";
+    console.log("No Pain No Gain");
   }
 });
