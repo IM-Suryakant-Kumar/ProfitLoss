bedec477cb80748a7eef5eaf7e5f8a7c2987d46d
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-02-14 14:22:51 +0530

    docs: Ex-07: Handling the corner cases

--- app.js
+++ app.js
@@ -9,13 +9,18 @@ tellMeBtn.addEventListener("click", () => {
   const currPriceValue = Number(currPrice.value);
   const numOfStocksValue = Number(numOfStocks.value);
 
+  if (initPriceValue === 0 || currPriceValue === 0 || numOfStocksValue === 0) {
+    alert("Please fill out all Fields");
+    return;
+  }
+
   if (initPriceValue > currPriceValue) {
     const loss = (initPriceValue - currPriceValue) * numOfStocksValue;
-    const lossPercentage = (loss / initPriceValue) * 100;
+    const lossPercentage = ((loss / initPriceValue) * 100).toFixed(2);
     console.log("loss", loss, "lossPercentage", lossPercentage);
   } else if (initPriceValue < currPriceValue) {
     const profit = (currPriceValue - initPriceValue) * numOfStocksValue;
-    const profitPercentage = (profit / initPriceValue) * 100;
+    const profitPercentage = ((profit / initPriceValue) * 100).toFixed(2);
     console.log("profit", profit, "profitPercentage", profitPercentage);
   }
 });
