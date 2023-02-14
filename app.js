ddea1afc72740bf57fbf8dbb262b1a5ed197229c
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-02-14 13:14:41 +0530

    docs: ex04: selecting Elements using querySelector

---
+++ app.js
@@ -0,0 +1,7 @@
+const initPrice = document.querySelector("#initial-price");
+const numOfStocks = document.querySelector("#quantity");
+const currPrice = document.querySelector("#current-price");
+const tellMeBtn = document.querySelector("#tell-me-btn");
+const output = document.querySelector("#output");
+
+console.log(initPrice, numOfStocks, currPrice, tellMeBtn, output);
