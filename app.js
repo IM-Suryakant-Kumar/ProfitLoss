8eacc546f112e05c087505c8348e02253d3d8034
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-02-14 13:46:02 +0530

    docs: ex05: create Function for Calculating Loss and Profit

--- app.js
+++ app.js
@@ -4,4 +4,16 @@ const currPrice = document.querySelector("#current-price");
 const tellMeBtn = document.querySelector("#tell-me-btn");
 const output = document.querySelector("#output");
 
-console.log(initPrice, numOfStocks, currPrice, tellMeBtn, output);
+function calculateProfitLoss(initPrice, currPrice, numOfStocks) {
+  if (initPrice > currPrice) {
+    const loss = (initPrice - currPrice) * numOfStocks;
+    const lossPercentage = (loss / initPrice) * 100;
+    console.log("loss", loss, 'lossPercentage', lossPercentage);
+  } else if (initPrice < currPrice) {
+    const profit = (currPrice - initPrice) * numOfStocks;
+    const profitPercentage = (profit / initPrice) * 100;
+    console.log('profit', profit,'profitPercentage', profitPercentage);
+  }
+}
+
+calculateProfitLoss(5, 10, 5);
