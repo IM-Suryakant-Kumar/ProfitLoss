5684596bc471bfc7e99fc34ebb6fdcf1a73ea52d
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-02-14 14:07:12 +0530

    docs: ex06: adding event listeners

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const currPrice = document.querySelector("#current-price");
 const tellMeBtn = document.querySelector("#tell-me-btn");
 const output = document.querySelector("#output");
 
-function calculateProfitLoss(initPrice, currPrice, numOfStocks) {
-  if (initPrice > currPrice) {
-    const loss = (initPrice - currPrice) * numOfStocks;
-    const lossPercentage = (loss / initPrice) * 100;
-    console.log("loss", loss, 'lossPercentage', lossPercentage);
-  } else if (initPrice < currPrice) {
-    const profit = (currPrice - initPrice) * numOfStocks;
-    const profitPercentage = (profit / initPrice) * 100;
-    console.log('profit', profit,'profitPercentage', profitPercentage);
-  }
-}
+tellMeBtn.addEventListener("click", () => {
+  const initPriceValue = Number(initPrice.value);
+  const currPriceValue = Number(currPrice.value);
+  const numOfStocksValue = Number(numOfStocks.value);
 
-calculateProfitLoss(5, 10, 5);
+  if (initPriceValue > currPriceValue) {
+    const loss = (initPriceValue - currPriceValue) * numOfStocksValue;
+    const lossPercentage = (loss / initPriceValue) * 100;
+    console.log("loss", loss, "lossPercentage", lossPercentage);
+  } else if (initPriceValue < currPriceValue) {
+    const profit = (currPriceValue - initPriceValue) * numOfStocksValue;
+    const profitPercentage = (profit / initPriceValue) * 100;
+    console.log("profit", profit, "profitPercentage", profitPercentage);
+  }
+});
